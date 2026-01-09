2216e863d64f1a3c0964a6966c9fc7c16742bbd7
sujal-shrestha <sujalwork69@gmail.com>
2026-01-10 00:20:25 +0545

    Update venue model parsing for backend response

--- lib/data/models/venue_model.dart
+++ lib/data/models/venue_model.dart
@@ -1,22 +1,20 @@
+// lib/data/models/venue_model.dart
 class VenueModel {
   final String id;
   final String name;
-  final String type;
   final String logoUrl;
 
   VenueModel({
     required this.id,
     required this.name,
-    required this.type,
     required this.logoUrl,
   });
 
   factory VenueModel.fromJson(Map<String, dynamic> json) {
     return VenueModel(
-      id: json['_id'] ?? json['id'] ?? '',
-      name: json['name'] ?? '',
-      type: json['type'] ?? '',
-      logoUrl: json['logoUrl'] ?? '',
+      id: (json['_id'] ?? json['id'] ?? '').toString(),
+      name: (json['name'] ?? '').toString(),
+      logoUrl: (json['logoUrl'] ?? '').toString(),
     );
   }
 }
